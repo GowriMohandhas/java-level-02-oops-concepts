f3c9b786bbf02d94e817279976cd5a23c36a4634
GowriMohandhas <gowripriya.m2007@gmail.com>
2026-05-09 18:56:07 +0530

    prgm 9

---
+++ Bsc.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Bsc {
+  void java() {
+    System.out.println("Java program");
+  }
+}
+
+class Aiml extends Bsc {
+  void os() {
+    System.out.println("Operating System");
+  }
+}
+
+class Bca extends Aiml {
+  void cws() {
+    System.out.println("Cloud web Service");
+  }
+}
