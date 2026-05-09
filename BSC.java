26191feb09e9a9b1a2b1383a7877f2a2dc8b0673
GowriMohandhas <gowripriya.m2007@gmail.com>
2026-05-09 18:53:47 +0530

    prgm 8

---
+++ BSC.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+
+class BSC {
+    void java() {
+        System.out.println("Java Program");
+    }
+   void os() {
+        System.out.println("Operating System");
+    }
+}
+class BCA extends BSC {
+    void cws() {
+        System.out.println("Cloud Web Service");
+    }
+}
