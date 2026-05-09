5bde4c472a35a66c8ab588f85d94c569718b7fba
GowriMohandhas <gowripriya.m2007@gmail.com>
2026-05-09 18:54:28 +0530

    prgm 7

---
+++ Car.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class Car { 
+
+    String brand;
+    int year;
+
+    Car(String b, int y) {
+        brand = b;
+        year = y;
+    }
+
+void display() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Year: " + year);
+    }
+}
